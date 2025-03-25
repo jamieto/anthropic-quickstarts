56f3d93e015f90266e882e3f56a85fcd251357a1
Jamie <jamieto@users.noreply.github.com>
2025-03-26 06:04:39 +1100

    Updated system prompt

--- computer-use-demo/computer_use_demo/loop.py
+++ computer-use-demo/computer_use_demo/loop.py
@@ -72,6 +72,7 @@ SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 <IMPORTANT>
 * When using Firefox, if a startup wizard appears, IGNORE IT.  Do not even click "skip this step".  Instead, click on the address bar where it says "Search or enter address", and enter the appropriate search term or URL there.
 * If the item you are looking at is a pdf, if after taking a single screenshot of the pdf it seems that you want to read the entire document instead of trying to continue to read the pdf from your screenshots + navigation, determine the URL, use curl to download the pdf, install and use pdftotext to convert it to a text file, and then read that text file directly with your StrReplaceEditTool.
+* When inputting text into a text field, you must confirm that the text has been entered correctly by checking the text field after you have entered the text. Make sure to check for any extra spaces or characters that may have been added. Make sure to check for any typos. Make sure to check for any missing characters. Only proceed once you have confirmed that the text has been entered correctly.
 </IMPORTANT>"""
 
 
@@ -438,7 +439,7 @@ def _inject_prompt_caching(
                     {"type": "ephemeral"}
                 )
             else:
-                content[-1].pop("cache_control", None)
+                content[-1].pop("cache_control", None)  # type: ignore
                 # we'll only every have one extra turn per loop
                 break
 
